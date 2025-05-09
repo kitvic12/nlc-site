27b4d4bca0c88dc274faa39361dd125bf05064c1
kitvic <kitvic12@gmail.com>
2025-05-09 12:03:56 +0300

    4d try

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function toggleContent(button) {
-    // Переключаем класс active для кнопки
     button.classList.toggle('active');
-    
-    // Получаем связанный контент
     const content = button.nextElementSibling;
-    
-    // Переключаем видимость контента
     if (content.style.display === 'block') {
       content.style.display = 'none';
-      button.style.background = '#4285f4';
+      button.style.background = '#79857d'; 
+      button.style.borderColor = '#5a665e'; 
     } else {
       content.style.display = 'block';
-      button.style.background = '#3367d6';
+      button.style.background = '#5a665e';
+      button.style.borderColor = '#3d4741';
     }
-    
-    // Отменяем стандартное поведение (на всякий случай)
     return false;
   }

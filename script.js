f90e65df4577c198980b9456f68196f076980512
kitvic <kitvic12@gmail.com>
2025-05-10 00:26:32 +0300

    update

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function toggleContent(button) {
     }
     return false;
   }
+
+
+function highlightCurrentPageButton() {
+const currentPath = window.location.pathname.split('/').pop();
+const navButtons = document.querySelectorAll('.nav-btn');
+navButtons.forEach(button => {
+    const buttonPath = button.getAttribute('data-href').split('/').pop();  
+    if (buttonPath === currentPath) {
+    button.classList.remove('btn-success');
+    button.classList.add('btn-light');
+    button.style.color = 'black';
+    } else {
+    button.classList.remove('btn-light');
+    button.classList.add('btn-success');
+    button.style.color = 'white';
+    }
+});
+}

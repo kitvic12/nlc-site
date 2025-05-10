5d21bcef919e1af78ad4f8524b3ca1b46608ff33
kitvic <kitvic12@gmail.com>
2025-05-10 03:09:48 +0300

    try

--- script/script_commits.js
+++ script/script_commits.js
@@ -1,4 +1,14 @@
-import {firebaseConfig} from "./fb_config";
+const firebaseConfig = {
+    apiKey: "AIzaSyC6VJP5KMoKlBPiKmZlada4sV7aypDxoSE",
+    authDomain: "commits-for-nlc-site.firebaseapp.com",
+    databaseURL: "https://commits-for-nlc-site-default-rtdb.firebaseio.com",
+    projectId: "commits-for-nlc-site",
+    storageBucket: "commits-for-nlc-site.firebasestorage.app",
+    messagingSenderId: "30201732398",
+    appId: "1:30201732398:web:86aa33a647a53621deaebf",
+    measurementId: "G-YWFJ2FJT9Q"
+  };
+
 
   firebase.initializeApp(firebaseConfig);
   const database = firebase.database();
